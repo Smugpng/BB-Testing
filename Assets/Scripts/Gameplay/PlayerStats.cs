6fb4c0cf0eff0825cd3e0a4cacce6a4b90ef90eb
Smug <zikadika2@gmail.com>
2026-03-11 17:04:45 -0400

    Dad and player testing

--- Assets/Scripts/Gameplay/PlayerStats.cs
+++ Assets/Scripts/Gameplay/PlayerStats.cs
@@ -5,32 +5,75 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerStats : MonoBehaviour
 {
-    public float susMeter;
-    [SerializeField]private PlayerControls pc;
+    //Suspicion meter and when to check
+    public float susMeter,susMeterMax;
+
+    //Dad contorls
+    [SerializeField] private bool isChecking;
+
+    //Player Data
+    private float cookieMultiplier = 1f;
+    //Other Scripts + this one
+    [SerializeField] private PlayerControls pc;
+    public static PlayerStats instance;
 
     private void Awake()
     {
+        if(instance == null)
+        {
+            instance = new PlayerStats();
+        }
         pc = GetComponentInChildren<PlayerControls>();
+        InvokeRepeating("SusMeter", .1f, .5f);
     }
-    private void Update() //Simple state check to simple suspicion meter
+    public void resetData()
     {
-
-        switch (pc.moveState)
+        susMeter = 0f;
+        cookieMultiplier = 1f;
+        isChecking = false;
+    }
+    public void CookiePickup() { cookieMultiplier = 2f; } //When player picks up cookie and heads back make it harder to get to the end?
+ 
+    public void SusMeter()
+    {
+        if(susMeter <= susMeterMax && !isChecking)
         {
-            case MovementState.Idle:
-                susMeter += 0;
-                return;
-            case MovementState.Forward:
-                susMeter += 1;
-                break;
-            case MovementState.Backward:
-                susMeter += .5f;
-                break;
+            switch (pc.moveState)
+            {
+                case MovementState.Idle:
+                    susMeter += 0;
+                    return;
+                case MovementState.Forward:
+                    susMeter += (1* cookieMultiplier);
+                    break;
+                case MovementState.Backward:
+                    susMeter += (.5f * cookieMultiplier);
+                    break;
+            }
+            if (pc.isSprinting)
+            {
+                susMeter += (2 * cookieMultiplier);
+            }
         }
-        if (pc.isSprinting)
+        else if(isChecking)
         {
-            susMeter += 2;
+            return;
         }
-
+        else
+        {
+            CheckSus();
+        }
+        
+    }
+    public void ResetSus()
+    {
+        isChecking = false;
+    }
+    public void CheckSus()
+    {
+        susMeter = 0;
+        isChecking = true;
+        Debug.Log("LOOKING!");
+        Invoke("ResetSus",5f);
     }
 }
